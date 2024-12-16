50360b79eda892644a7f0354395cf1adf38ce187
ViktorOnyedim <viktoronyedim@gmail.com>
2024-12-16 21:59:26 +0100

    created models

--- backend/quiz/models.py
+++ backend/quiz/models.py
@@ -1,24 +1,46 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import timedelta
+
+QUESTION_TYPE = (
+    ("single choice", "single choice")
+    ("multiple choice", "multiple choice"),
+    ("text", "text"),
+    ("boolean", "boolean")
+)
 
 # Create your models here.
 class Organization(models.Model):
     name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    address = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 class QuizCreator(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user.username
+
 class Quiz(models.Model):
     title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    time_limit = models.DurationField(default=timedelta(minues=30))
     creator = models.ForeignKey(QuizCreator, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 class Question(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name="questions")
     text = models.CharField(max_length=300)
-    order = models.IntegerField(default=0)
+    type = models.CharField(choices=QUESTION_TYPE, default="single choice")
+    # order = models.IntegerField(default=0)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="choices")
