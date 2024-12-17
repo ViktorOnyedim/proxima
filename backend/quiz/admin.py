21358d8d104aade31d182f01ca4d6887fd6f5ce8
ViktorOnyedim <viktoronyedim@gmail.com>
2024-12-17 14:02:58 +0100

    Registered models

--- backend/quiz/admin.py
+++ backend/quiz/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from quiz import models
 
 # Register your models here.
+admin.site.register(models.Organization)
+admin.site.register(models.QuizCreator)
+admin.site.register(models.Quiz)
+admin.site.register(models.Question)
+admin.site.register(models.Answer)
+admin.site.register(models.Choice)
+admin.site.register(models.Participant)
+
