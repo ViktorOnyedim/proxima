aa25db7039f754a1a873dbfb74c3f767f6724f49
ViktorOnyedim <viktoronyedim@gmail.com>
2024-12-17 15:56:46 +0100

    Modified Choice and Answer Models

--- backend/quiz/models.py
+++ backend/quiz/models.py
@@ -38,31 +38,45 @@ class Quiz(models.Model):
     def __str__(self):
         return self.title
 
+
 class Question(models.Model):
     text = models.TextField()
-    type = models.CharField(max_length=2, choices=QUESTION_TYPE, default="single choice")
+    type = models.CharField(max_length=2, choices=QUESTION_TYPE, default="SC")
     order = models.PositiveIntegerField()
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name="questions")
 
     def __str__(self):
         return f"Question {self.id}: {self.text}"
 
-class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="answers")
-    text = models.CharField(max_length=200)
-    is_correct = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.text
-
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="choices")
-    text = models.CharField(max_length=200)
+    text = models.CharField(max_length=200) # text of the choice
     is_correct = models.BooleanField(default=False)
 
     def __str__(self):
         return self.text
+
+
+class ParticipantAnswer(models.Model):
+    participant = models.ForeignKey('Participant', on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="answers")
+    choice = models.ForeignKey(Choice, null=True, blank=True, on_delete=models.CASCADE) # for SC/MC/B(boolean)
+    text_response = models.CharField(max_length=200) # For text questions
+    is_correct = models.BooleanField(default=False) # track if answer is the correct one
+
+    def save(self, *args, **kwargs):
+        if self.choice:
+            self.is_correct = self.choice.is_correct
+        elif self.text_response:
+            self.is_correct = self.text_response.strip().lower() == self.question.text.strip().lower()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        if self.choice:
+            return f"Answer to {self.question.text}: {self.choice.text}"
+        return f"Answer to {self.question.text}: {self.text_response}"
+
 
 class Participant(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -71,4 +85,4 @@ class Participant(models.Model):
     is_completed = models.BooleanField(default=False)
     
     def __str__(self):
-        return f"{self.user.username} - {self.quiz.title}"
+        return f"{self.user.username}"
