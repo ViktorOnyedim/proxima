a4a8a286e998408b1a0f1553980839060d999c6e
ViktorOnyedim <viktoronyedim@gmail.com>
2024-12-19 14:16:52 +0100

    registered rest_framework app

--- backend/proxima/settings.py
+++ backend/proxima/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "quiz",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,9 @@ STATIC_ROOT = BASE_DIR / "static"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ]
+}
