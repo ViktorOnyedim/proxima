a8035e668157555285cef1d0bd8aeea441c78767
ViktorOnyedim <viktoronyedim@gmail.com>
2024-12-20 03:56:33 +0100

    Registered QuizResult model

--- backend/quiz/admin.py
+++ backend/quiz/admin.py
@@ -6,7 +6,7 @@ admin.site.register(models.Organization)
 admin.site.register(models.QuizCreator)
 admin.site.register(models.Quiz)
 admin.site.register(models.Question)
+admin.site.register(models.QuizResult)
 admin.site.register(models.ParticipantAnswer)
 admin.site.register(models.Choice)
 admin.site.register(models.Participant)
-
