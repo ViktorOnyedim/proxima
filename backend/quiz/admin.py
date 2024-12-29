f2faf3f9b190df553ae3bccfc2cdcc4cfc706802
ViktorOnyedim <viktoronyedim@gmail.com>
2024-12-29 21:59:28 +0100

    Added list_display for Quiz and Choice admin

--- backend/quiz/admin.py
+++ backend/quiz/admin.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from quiz import models
 
 # Register your models here.
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_active')
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('question', 'is_correct', 'text')
+
 admin.site.register(models.Organization)
 admin.site.register(models.QuizCreator)
-admin.site.register(models.Quiz)
+admin.site.register(models.Quiz, QuizAdmin)
 admin.site.register(models.Question)
 admin.site.register(models.QuizResult)
 admin.site.register(models.ParticipantAnswer)
-admin.site.register(models.Choice)
+admin.site.register(models.Choice, ChoiceAdmin)
 admin.site.register(models.Participant)
