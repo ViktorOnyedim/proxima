a6e8a3b958a0973278613a1196617e458ec80563
ViktorOnyedim <viktoronyedim@gmail.com>
2024-12-30 17:00:45 +0100

    Installed react for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-red-500 text-5xl text-center">Proxima</h1>
+    </>
+  )
+}
+
+export default App
