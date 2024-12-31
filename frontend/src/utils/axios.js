1b23d0b801757bd545c19c1a4eaecf479193221a
ViktorOnyedim <viktoronyedim@gmail.com>
2024-12-31 17:15:54 +0100

    created utils to connect to api using axios

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,5 +1,13 @@
 import axios from "axios";
+import { API_BASE_URL } from "./constants";
 
 const apiInstance = axios.create({
-    baseURL: 
-})
+    baseURL: API_BASE_URL,
+    timeout: 10000,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "Application/json",
+    },
+});
+
+export default apiInstance;
