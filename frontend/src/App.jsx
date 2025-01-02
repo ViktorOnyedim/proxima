a87dde20df8280d74b570185f2fcbf79d2d5bc92
ViktorOnyedim <viktoronyedim@gmail.com>
2025-01-02 11:51:55 +0100

    Added routing for the views

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
-// import { useState } from 'react'
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import QuizApp from "./components/QuizApp";
+import Home from "./components/Home";
+
 
 function App() {
 
   return (
     <>
       <h1 className="text-red-500 text-5xl text-center">Proxima</h1>
+      
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/quiz/" element={<QuizApp />} />
+          </Routes>
+      </BrowserRouter>
     </>
   )
 }
