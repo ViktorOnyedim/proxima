1e5e448e14d2cef09c75d440465ef52ada024f36
ViktorOnyedim <viktoronyedim@gmail.com>
2025-01-15 11:10:31 +0100

    added frontend api routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,12 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import QuizApp from "./components/QuizApp";
 import Home from "./components/Home";
+import QuizList from "./components/QuizList";
+// import QuizResults from "./components/QuizResults";
+import Login from "./components/Login";
+import Register from "./components/Register"
+import QuizResult from "./components/QuizResult";
+// import PrivateRoute from "./components/PrivateRoute";
 
 
 function App() {
@@ -12,7 +18,13 @@ function App() {
       <BrowserRouter>
           <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/quiz/" element={<QuizApp />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/quiz/" element={<QuizList />} />
+              <Route path="/quiz/:id" element={<QuizApp />} />
+              <Route path="/result/" element={<QuizResult />} />
+
+              {/* <Route path="/quiz-result/" element={<QuizResults />} /> */}
           </Routes>
       </BrowserRouter>
     </>
